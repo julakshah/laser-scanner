b5d05faeed38e451f6c5fc36db37b6bb5be2393f
julakshah <julakshah@gmail.com>
2025-09-27 18:59:06 -0400

    arduino logging code testing serial_read output

---
+++ visualize.py
@@ -0,0 +1,39 @@
+import serial
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+scanning_flag = False
+
+class scanMan():
+  def __init__(self)
+    self.arduino = get_arduino()
+
+  def get_arduino():
+    try:
+        arduino = serial.Serial(port="COM4", baudrate=9600, timeout=0.1)
+    except SerialTimeoutException:
+        print("connection to arduino couldn't be esetablished")
+    return arduino
+
+  def begin_program():
+    arduino.write("run".encode("utf-8"))
+
+  def write_data():
+    """writes data taken from the serial port into a csv file"""
+    with open("scanner_data.csv", "w", newline="") as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=",")
+        while scanning_flag:
+            serial_data = arduino.readline()
+            print(serial_data)
+
+
+def main():
+  """runs to program in it's entirety"""
+    minion = scanMan()
+    begin_program()
+
+
+
+if __name__ == "__main__":
+    main()
