2844be82cc0db48ef9521d781fe0be953f79cf95
julakshah <julakshah@gmail.com>
2025-09-27 21:21:59 -0400

    serial logging of data code

---
+++ scanner/scanner.ino
@@ -0,0 +1,66 @@
+#include <Servo.h>
+
+Servo bottomservo;  // create servo object to control a servo
+Servo topservo;
+
+// variables to store the servo positions/sensor value & pin
+int bottompos = 90;    // 100 left, 60 right 90 d
+int toppos = 55; // 20 top, 80 bottom 55 d
+const int BOTTOM_ZERO = 90;
+const int TOP_ZERO = 55;
+int sensorValue = 0;
+int sensorPin = A0;
+bool runFlag = false;
+
+void setup() {
+  bottomservo.attach(11);  // attaches the servo on pin 9 to the servo object
+  bottomservo.write(bottompos);
+  topservo.attach(3);
+  topservo.write(toppos);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+// 3D Scanning
+
+void loop() {
+  if (!runFlag) {
+      String serial_string = Serial.readString();
+      serial_string.trim();
+      if (serial_string == "run") {
+        runFlag = true;
+        delay(200);
+      }
+  } else if (runFlag) {
+    scan_sequence();
+    delay(200);
+    runFlag = false;
+  }
+}
+
+void scan_sequence() {
+  for (bottompos = 60; bottompos <= 100; bottompos += 1) {
+    bottomservo.write(bottompos);
+    delay(150);
+    for (toppos = 20; toppos <= 80; toppos += 1){
+      topservo.write(toppos);
+      delay(15);
+      Serial.print(analogRead(sensorPin));
+      Serial.print(" ");
+      Serial.print(toppos - TOP_ZERO);
+      Serial.print(" ");
+      Serial.println(bottompos - BOTTOM_ZERO);
+      delay(2);
+    }
+  }
+  Serial.println("scan done");
+}
+
+// Calibration
+
+// void loop() {
+//   bottomservo.write(bottompos);
+//   topservo.write(toppos);
+//   Serial.println(analogRead(sensorPin));
+// }
--- visualize.py
+++ visualize.py
@@ -3,36 +3,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-scanning_flag = False
 
-class scanMan():
-  def __init__(self)
-    self.arduino = get_arduino()
+class scanMan:
+    def __init__(self, port_string):
+        self.arduino = self.get_arduino(port_string)
+        self.scanning_flag = False
 
-  def get_arduino():
-    try:
-        arduino = serial.Serial(port="COM4", baudrate=9600, timeout=0.1)
-    except SerialTimeoutException:
-        print("connection to arduino couldn't be esetablished")
-    return arduino
+    def get_arduino(self, port_string):
+        arduino = serial.Serial(port=port_string, baudrate=9600, timeout=1)
+        return arduino
 
-  def begin_program():
-    arduino.write("run".encode("utf-8"))
+    def begin_program(self):
+        self.scanning_flag = True
+        self.arduino.write("run".encode("utf-8"))
 
-  def write_data():
-    """writes data taken from the serial port into a csv file"""
-    with open("scanner_data.csv", "w", newline="") as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=",")
-        while scanning_flag:
-            serial_data = arduino.readline()
-            print(serial_data)
+    def write_data(self):
+        """writes data taken from the serial port into a csv file"""
+        with open("scanner_data.csv", "w", newline="") as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=",")
+            while self.scanning_flag:
+                serial_data = self.arduino.readline().decode("utf-8")
+                serial_data = serial_data.strip("b'")
+                serial_data = serial_data.strip("\r\n'")
+
+                print(serial_data)
+                if serial_data == "scan done":
+                    self.scanning_flag = False
+                    break
+                if serial_data != "":
+                    line_data = serial_data.split()
+                    csvwriter.writerow([line_data[0], line_data[1], line_data[2]])
+        print("csv written")
+
+
+def flatten_data(csv_path="~/Documents/GitHub/laser-scanner/scanner_data"):
+    """converts to data from polar to cartesian coordinates"""
+    print("flattening data")
 
 
 def main():
-  """runs to program in it's entirety"""
-    minion = scanMan()
-    begin_program()
-
+    """runs to program in it's entirety"""
+    minion = scanMan("/dev/cu.usbmodemB43A4536DECC2")
+    minion.begin_program()
+    minion.write_data()
 
 
 if __name__ == "__main__":
