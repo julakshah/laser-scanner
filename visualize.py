9ae683eb82389f724f92ee6fe66d703b66a13b67
julakshah <julakshah@gmail.com>
2025-09-27 23:06:00 -0400

    first full workflow

--- visualize.py
+++ visualize.py
@@ -45,11 +45,11 @@ def calibrate_data(csv_path="scanner_data.csv"):
     REF_VOLT = 5  # voltage
     with open(csv_path, "r", newline="") as raw_data:
         reader = csv.reader(raw_data, delimiter=",")
-        with open("calibrated_scanner_data", "w", newline="") as calib_data:
+        with open("calibrated_scanner_data.csv", "w", newline="") as calib_data:
             writer = csv.writer(calib_data, delimiter=",")
             for row in reader:
                 scan_v = int(row[0]) / 1024 * REF_VOLT
-                y = (22 / scan_v) ^ (1 / 0.984)
+                y = 1 / (23.3 * scan_v)
                 writer.writerow([y, row[1], row[2]])
 
 
@@ -59,19 +59,22 @@ def flatten_data(csv_path="calibrated_scanner_data.csv"):
     REF_VOLT = 5  # voltage
     with open(csv_path, "r", newline="") as calib_data:
         reader = csv.reader(calib_data, delimiter=",")
-        with open("cartesian_scanner_data", "w", newline="") as cart_data:
-            writer = csv.writer(calib_data, delimiter=",")
+        with open("cartesian_scanner_data.csv", "w", newline="") as cart_data:
+            writer = csv.writer(cart_data, delimiter=",")
+            writer.writerow(["x", "y", "z"])
             for row in reader:
-                dist = row[0]
-                top_ang = (row[1] / 360) * 2 * np.pi  # convert toppos to rad
-                bot_ang = (row[2] / 360) * 2 * np.pi  # convert botpos to rad
-                x = dist * np.cos(top_ang) * np.sin(bot_ang)
-                y = dist * np.cos(top_ang) * np.cos(bot_ang)
-                z = dist * np.sin(top_ang)
+                dist = float(row[0])
+                top_ang = (int(row[1]) / 360) * 2 * np.pi  # convert toppos to rad
+                bot_ang = (int(row[2]) / 360) * 2 * np.pi  # convert botpos to rad
+                x = dist * float(np.cos(top_ang)) * float(np.sin(bot_ang))
+                y = dist * float(np.cos(top_ang)) * float(np.cos(bot_ang))
+                z = dist * float(np.sin(top_ang))
                 writer.writerow([x, y, z])
 
 
-def plotting():
+def calibration_plot():
+    """plots a plot of the calibration data along with the equation fitted
+    in excel"""
     x = np.linspace(8, 59, 600)
     y = 22 * np.power(x, -0.984)
     df = pd.read_csv("calibration_data.csv")
@@ -85,14 +88,30 @@ def plotting():
     plt.savefig("calibration.png")
 
 
+def scan_2d_plot():
+    """plots a 2d representation of the scan"""
+    print("plotting 2d representation")
+    x = np.linspace(8, 59, 600)
+    y = 22 * np.power(x, -0.984)
+    df = pd.read_csv("cartesian_scanner_data.csv")
+    print(df)
+    plt.scatter(df["x"], df["y"], label="2d scan")
+    plt.title("2D image")
+    plt.xlabel("x pos (in)")
+    plt.ylabel("y pos (in)")
+    plt.axes([0, 60, 0, 3])
+    plt.savefig("2d_image.png")
+
+
 def main():
     """runs to program in it's entirety"""
-    # minion = scanMan("/dev/cu.usbmodemB43A4536DECC2")
-    # minion.begin_program()
-    # minion.write_data()
-    plotting()
+    minion = scanMan("/dev/cu.usbmodemB43A4536DECC2")
+    minion.begin_program()
+    minion.write_data()
+    # calibration_plot()
     calibrate_data()
     flatten_data()
+    scan_2d_plot()
 
 
 if __name__ == "__main__":
