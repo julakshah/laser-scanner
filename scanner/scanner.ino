268aefd49613daf5807ddba05e2ccc29b0447fca
Lukas Littlejohn <LLittlejohn@olin.edu>
2025-09-28 00:23:09 -0400

    angled better

--- scanner/scanner.ino
+++ scanner/scanner.ino
@@ -5,7 +5,7 @@ Servo topservo;
 
 // variables to store the servo positions/sensor value & pin
 int bottompos = 90;    // 120 left, 60 right 90 d
-int toppos = 55; // 30 top, 80 bottom 55 d
+int toppos = 55; // 40 top, 80 bottom 55 d
 const int BOTTOM_ZERO = 90;
 const int TOP_ZERO = 55;
 const int MAX_SENSOR = 170; // 28"
@@ -45,7 +45,7 @@ void scan_sequence() {
   for (bottompos = 60; bottompos <= 120; bottompos += 1) {
     bottomservo.write(bottompos);
     delay(150);
-    for (toppos = 30; toppos <= 80; toppos += 1) {
+    for (toppos = 40; toppos <= 80; toppos += 1) {
       topservo.write(toppos);
       delay(15);
       if (analogRead(sensorPin) >= MAX_SENSOR && analogRead(sensorPin) <= MIN_SENSOR) {
